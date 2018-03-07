74a1c5e8f36f827d67b7c523cf7755ffafbaa30c
SRomero85 <37090124+SRomero85@users.noreply.github.com>
2018-03-07 14:25:53 -0300

    Add files via upload

---
+++ Codigo Funcional.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+
+#define RAD(a,b,c) (pow(b,2) - (4 * a*c))
+
+using namespace std;
+
+// este programa tiene todos los parentesis necesarios para dar una corecta solucion a la ecuacion de segundo grado
+
+int main() {
+
+	float a, b, c, d = 0, x1 = 0, x2 = 0;
+	char f;
+
+	cout << "Ingrese el valor de 'a': "; cin >> a;
+	cout << "Ingrese el valor de 'b': "; cin >> b;
+	cout << "Ingrese el valor de 'c': "; cin >> c;
+
+	d = RAD(a, b, c);
+
+		if( d >= 0 ) {
+
+			x1 = ((-b) + sqrt(RAD(a, b, c))) / (2 * a);
+
+			x2 = ((-b) - sqrt(RAD(a, b, c))) / (2 * a);
+
+			cout << "\nEl Resultado de 'x1' es :" << x1 << endl;
+			cout << "\nEl Resultado de 'x2' es :" << x2 << endl;
+
+			cout << "\nIngrese S para continuar: "; cin >> f;
+		} else {
+			cout << "\nEl problema no tiene solucion";
+			cout << "\nIngrese S para continuar: "; cin >> f;
+		}
+
+	return 0;
+}
